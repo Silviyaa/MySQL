e6d03559ad0c8b064f0854bf616093f91478c12f
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-01-27 17:44:43 +0200

    add new examples

--- 3. Built-in Functions/Lab/Built-in Functions - Exercise.sql
+++ 3. Built-in Functions/Lab/Built-in Functions - Exercise.sql
@@ -3,3 +3,18 @@
  from employees
  WHERE `first_name` like 'Sa%'
  ORDER BY `employee_id`;
+ 
+#Ex: 02. Find Names of All Employees by Last Name 
+ SELECT `first_name`, `last_name`
+ from employees
+ WHERE `last_name` like '%ei%'
+ ORDER BY `employee_id`;
+ 
+#Ex: 03. Find First Names of All Employess
+SELECT `first_name`
+from employees
+WHERE `department_id` IN (3,10) and `hire_date` BETWEEN(year(1995 and 2005))
+ORDER BY `employee_id`;
+   
+   
+   
