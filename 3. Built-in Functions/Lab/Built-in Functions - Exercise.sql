016f5999ace36acdd34a0bf3ab54645ba1a65ebe
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-01-30 15:45:47 +0200

    Update
    Add new exercise

--- 3. Built-in Functions/Lab/Built-in Functions - Exercise.sql
+++ 3. Built-in Functions/Lab/Built-in Functions - Exercise.sql
@@ -50,6 +50,38 @@ WHERE year(hire_date) > 2000;
 #Ex: 09.Length of Last Name
 SELECT `first_name`,`last_name`
 FROM employees
-WHERE char_length(`first_name`) IN (5) or char_length(`last_name`) in (5);
- 
+WHERE char_length(`last_name`) in (5);
 
+#Ex: 10.Countries Holding 'A' 3 or More Times 
+SELECT `country_name`,`iso_code`
+from countries
+WHERE `country_name` LIKE '%A%A%A%' 
+ORDER BY iso_code ASC;
+
+#Ex: 11.Mix of Peak and River Names 
+SELECT `peak_name`,`river_name`,
+LOWER(CONCAT(`peak_name`, SUBSTRING(`river_name`,2))) as 'mix'
+from `peaks`,`rivers`
+WHERE right(lower(`peak_name`),1 ) = left(lower(`river_name`),1 )
+ORDER BY mix;
+
+#Ex: 12. Games From 2011 and 2012 Year
+SELECT `name`, date_format(`start`,'%Y-%m-%d') as 'start'
+from games
+WHERE year(`start`) BETWEEN 2011 and 2012
+ORDER BY `start`, `name`
+limit 50;
+
+#Ex: 13. User Email Providers
+SELECT `user_name`,substring(`email`, locate('@',`email`)+1) AS 'email_provider'
+from users
+ORDER BY `email_provider`, `user_name`; 
+
+#Ex: 14. Get Users with IP Address Like Pattern
+SELECT `user_name`,`ip_address`
+from `users`
+WHERE `ip_address` LIKE '___.1%.%.___'
+ORDER BY `user_name`;
+
+#Ex: 15. Show All Games with Duration
+ 
