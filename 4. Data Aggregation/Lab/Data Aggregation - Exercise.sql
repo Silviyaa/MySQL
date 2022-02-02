285495b0703e1dc63769a09f0a928b4da1e8909e
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-02-02 18:12:29 +0200

    Add new exercises

--- 4. Data Aggregation/Lab/Data Aggregation - Exercise.sql
+++ 4. Data Aggregation/Lab/Data Aggregation - Exercise.sql
@@ -32,4 +32,39 @@ GROUP BY `deposit_group`
 ORDER BY `deposit_group` ASC;
 
 #Ex: 07. Deposits Filter
+SELECT `deposit_group`,sum(`deposit_amount`) as 'total_sum'
+FROM `wizzard_deposits`
+WHERE `magic_wand_creator` = 'Ollivander family'
+GROUP BY `deposit_group`
+HAVING `total_sum` < 150000
+ORDER BY  `total_sum` DESC;
+
+#Ex: 08. Deposit Charge
+SELECT `deposit_group`,`magic_wand_creator`,MIN(`deposit_charge`) as 'min_deposit_charge'
+FROM `wizzard_deposits`
+GROUP BY `deposit_group` , `magic_wand_creator` 
+ORDER BY `magic_wand_creator`asc,`deposit_group`asc;
+
+#Ex: 09. Age Groups
+SELECT (
+	CASE
+		WHEN `age` BETWEEN 0 and 10 THEN '[0-10]'
+		WHEN `age` BETWEEN 11 and 20 THEN '[11-20]'
+		WHEN `age` BETWEEN 21 and 30 THEN '[21-30]'
+		WHEN `age` BETWEEN 21 and 40 THEN '[31-40]'
+		WHEN `age` BETWEEN 41 and 50 THEN '[41-50]'
+		WHEN `age` BETWEEN 51 and 60 THEN '[51-60]'
+        ELSE '[61+]'
+    END
+) as 'age_group' , COUNT(*) as 'wizard_count'
+FROM `wizzard_deposits`
+GROUP BY `age_group`;
+
+#Ex: 10. First Letter
+SELECT LEFT(`first_name`,1) as 'first_letter'
+FROM `wizzard_deposits`
+WHERE `deposit_group` like 'Troll Chest'
+GROUP BY `first_letter`
+ORDER BY `first_letter` asc;
+
  
