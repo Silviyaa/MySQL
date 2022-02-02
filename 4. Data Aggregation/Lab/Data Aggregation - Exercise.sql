1148a0db476d682aa21cc2f390fc793d41620d9c
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-02-02 12:42:37 +0200

    Data Aggregation - Exercise

---
+++ 4. Data Aggregation/Lab/Data Aggregation - Exercise.sql
@@ -0,0 +1,28 @@
+#Ex: 1.Records' Count
+SELECT COUNT(*) as 'count' FROM `wizzard_deposits`;
+
+#Ex: 02. Longest Magic Wand
+SELECT MAX(`magic_wand_size`) as 'longest_magic_wand'
+FROM `wizzard_deposits`;
+
+#Ex: 03. Longest Magic Wand per Deposit Groups
+SELECT `deposit_group`, max(`magic_wand_size`) as 'max_size'
+FROM `wizzard_deposits` 
+GROUP BY `deposit_group`
+ORDER BY `max_size` ASC, `deposit_group` ASC;
+
+#Ex: 04. Smallest Deposit Group per Magic Wand Size
+SELECT `deposit_group`
+FROM `wizzard_deposits` 
+GROUP BY (`deposit_group`)
+ORDER BY AVG(`magic_wand_size`)
+LIMIT 1;
+
+#Ex: 05. Deposits Sum
+SELECT `deposit_group`, SUM(`deposit_amount`) as 'total_sum'
+FROM `wizzard_deposits`
+GROUP BY `deposit_group`
+ORDER BY `total_sum`ASC;
+
+#Ex: 06. Deposits Sum for Ollivander Family
+  
