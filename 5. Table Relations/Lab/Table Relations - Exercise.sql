6cc57604d7ade65eadcf89abb505c1b0255c5c81
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-02-09 21:58:32 +0200

    add exercise 3

--- 5. Table Relations/Lab/Table Relations - Exercise.sql
+++ 5. Table Relations/Lab/Table Relations - Exercise.sql
@@ -61,6 +61,20 @@ VALUE
 (105,'Model 3',2),
 (106,'Nova',3);
 
+#Ex: 03. Many-To-Many Relationship
+CREATE DATABASE `Many-To-Many`;
+
+CREATE TABLE exams (
+	exam_id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20)
+)AUTO_INCREMENT = 101;
+
+INSERT INTO exams(name)
+VALUES
+('Spring MVC'),
+('Neo4j'),
+('Oracle 11g');
+
 CREATE TABLE students(
 	student_id INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(20)
@@ -73,3 +87,26 @@ VALUES
 ('Toni'),
 ('Ron');
 
+CREATE TABLE students_exams(
+	student_id INT,
+    exam_id INT,
+    CONSTRAINT pk_students_exams
+    PRIMARY KEY (student_id, exam_id),
+    
+    CONSTRAINT fk_students_exams_students
+    FOREIGN KEY (student_id)
+    REFERENCES students (student_id),
+    
+    CONSTRAINT fk_students_exams_exams
+    FOREIGN KEY (exam_id)
+    REFERENCES exams (exam_id)
+);
+
+INSERT INTO students_exams(student_id,exam_id)
+VALUES
+(1,101),
+(1,102),
+(2,101),
+(3,103),
+(2,102),
+(2,103);
