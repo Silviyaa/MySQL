b5df5d79d37f3a7cf0937afaca8af0c61080142a
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-02-09 18:36:43 +0200

    Add ex. 2

--- 5. Table Relations/Lab/Table Relations - Exercise.sql
+++ 5. Table Relations/Lab/Table Relations - Exercise.sql
@@ -29,4 +29,47 @@ values
 ('Yana',60200.00,101);
 
 # 2.One-To-Many Relationship 
+CREATE DATABASE `one-to-many`;
+
+CREATE table manufacturers(
+	manufacturer_id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20),
+    established_on DATE
+)AUTO_INCREMENT = 1;
+
+INSERT INTO manufacturers(manufacturer_id,`name`,established_on)
+VALUE
+(1,'BMW','1916-03-01'),
+(2,'Tesla','2003-01-01'),
+(3,'Lada','1966-05-01');
+
+CREATE TABLE models(
+	model_id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20) NOT NULL,
+    manufacturer_id INT,
+    CONSTRAINT fk_models_manufacturers
+    FOREIGN KEY (manufacturer_id)
+    REFERENCES manufacturers (manufacturer_id)
+);
+
+INSERT INTO models(model_id,`name`,manufacturer_id)
+VALUE
+(101,'X1',1),
+(102,'i6',1),
+(103,'Model S',2),
+(104,'Model X',2),
+(105,'Model 3',2),
+(106,'Nova',3);
+
+CREATE TABLE students(
+	student_id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(20)
+)AUTO_INCREMENT =1;
+    
+ALTER TABLE students AUTO_INCREMENT = 1;
+INSERT INTO students(`name`)
+VALUES
+('Mila'),
+('Toni'),
+('Ron');
 
