fdcd095d530d1dbe68028084a83a7ce29032fc25
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-02-10 11:56:20 +0200

    add exercise 5

--- 5. Table Relations/Lab/Table Relations - Exercise.sql
+++ 5. Table Relations/Lab/Table Relations - Exercise.sql
@@ -138,13 +138,13 @@ REFERENCES teachers (teacher_id);
 CREATE DATABASE online_store;
 CREATE TABLE cities(
 	city_id INT(11) PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NULL
+    name VARCHAR(50)
 );
 
 CREATE TABLE customers(
 	customer_id INT(11) PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(50) NULL,
-    birthday DATE NULL,
+	name VARCHAR(50),
+    birthday DATE,
     city_id INT(11),
     
     CONSTRAINT fk_customers_cities
@@ -162,4 +162,33 @@ CREATE TABLE orders(
 );
 
 
+CREATE TABLE item_types(
+	item_type_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name varchar(50)
+);
 
+CREATE TABLE items (
+	item_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name varchar(50),
+    item_type_id INT(11),
+    
+    CONSTRAINT fk_items_item_types
+    FOREIGN KEY (item_type_id)
+    REFERENCES item_types (item_type_id)
+);
+
+CREATE TABLE order_items(
+	order_id INT(11),
+    item_id INT(11),
+    CONSTRAINT pk_order_items
+    PRIMARY KEY (order_id, item_id),
+    
+    CONSTRAINT fk_order_items_items
+    FOREIGN KEY (item_id)
+    REFERENCES items (item_id),
+    
+    CONSTRAINT fk_order_items_orders
+    FOREIGN KEY (order_id)
+    REFERENCES orders (order_id)
+);
+ 
