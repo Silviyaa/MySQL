98b05143fbde7f2c779cddf32562beaedbb25b48
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2022-02-10 12:44:58 +0200

    add Ex: 09. Peaks in Rila

--- 5. Table Relations/Lab/Table Relations - Exercise.sql
+++ 5. Table Relations/Lab/Table Relations - Exercise.sql
@@ -241,3 +241,11 @@ CREATE TABLE agenda(
     FOREIGN KEY (student_id)
     REFERENCES students (student_id)
 );
+
+#Ex: 09. Peaks in Rila
+SELECT m.mountain_range , p.peak_name, p.elevation 
+FROM mountains AS m
+JOIN peaks AS p 
+ON p.mountain_id = m.id
+WHERE m.mountain_range = 'Rila'
+ORDER BY p.elevation DESC;
